ab2cbf96fe9e2084ecbd718130ddb35557f83814
Anderson Dutra <andersonrdutra7@gmail.com>
2023-11-22 21:13:11 -0300

    22/11
    adicionado accounts

--- app_biblioteca/urls.py
+++ app_biblioteca/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="home"),
-    path("login/", views.login, name="login"),
-    path("register-user/", views.register_user, name="register-user"),
     path("user/", views.login, name="user"),
     path("add-books/", views.add_books, name="add-books"),
     path("book-detail/<int:id>/", views.book_detail, name="book-detail"),
